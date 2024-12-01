7338708a1d57c78eb6d180a2e2651cccb80fc5e4
Robin Gjølstad <robingjolstad@gmail.com>
2024-12-01 13:20:49 +0100

    Update templates

---
+++ day-1/src/part1.rs
@@ -0,0 +1,13 @@
+pub fn process(input: Into<String>) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() {
+        assert_eq!(process("".to_string()), 0);
+    }
+}
